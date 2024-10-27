7c52894a7ad9a0dbbcc6af1499e04a32cc2a1f1d
Lucas Ciampi <lucas_ciampi@hotmail.com>
2024-10-27 15:13:11 -0300

    Mudança no app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import MenuPrincipal from './components/MenuPrincipal/MenuPrincipal';
 import Grafico from './components/Grafico/Grafico';
 import axios from 'axios';
+import './App.css'; // Importar o arquivo CSS
 
 const App = () => {
   const [symbol, setSymbol] = useState("");
@@ -17,13 +18,11 @@ const App = () => {
       });
       const data = response.data;
   
-      // Formatar os dados para o gráfico de boxplot
       const formattedData = data.map(item => ({
         x: new Date(item.date).toLocaleDateString(),
-        y: [item.low, item.open, item.close, item.high, item.high] // Estrutura do boxplot: [low, q1, median, q3, high]
+        y: [item.low, item.open, item.close, item.high, item.high]
       }));
   
-      // Formatar os outliers usando o volume
       const formattedOutliers = data.map(item => ({
         x: new Date(item.date).toLocaleDateString(),
         y: item.volume
@@ -32,32 +31,30 @@ const App = () => {
       setChartData(formattedData);
       setOutliersData(formattedOutliers);
       
-      console.log(data);
-      console.log(formattedData);
-      console.log(formattedOutliers);
-  
     } catch (error) {
       console.error("Erro ao buscar dados", error);
     }
   };
   
-  
-
   return (
-    <div>
-      <MenuPrincipal
-        symbol={symbol}
-        fromDate={fromDate}
-        toDate={toDate}
-        setSymbol={setSymbol}
-        setFromDate={setFromDate}
-        setToDate={setToDate}
-        fetchData={fetchData}
-      />
-      <Grafico
-        chartData={chartData}
-        outliersData={outliersData}
-      />
+    <div className="container_principal">
+      <div className="menu_container">
+        <MenuPrincipal
+          symbol={symbol}
+          fromDate={fromDate}
+          toDate={toDate}
+          setSymbol={setSymbol}
+          setFromDate={setFromDate}
+          setToDate={setToDate}
+          fetchData={fetchData}
+        />
+      </div>
+      <div className="grafico_container">
+        <Grafico
+          chartData={chartData}
+          outliersData={outliersData}
+        />
+      </div>
     </div>
   );
 };
